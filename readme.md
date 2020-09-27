73d85f749639622c1b3522f013427024694db912
Walter Winser <turtle89431@gmail.com>
2020-09-26 20:45:22 -0700

    added Jquery,popper and bootstrap

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # Riot Socket.io and Express.js
 - must use nodemon during dev
+- routing handled in server.js
+- includes Jquery,popper and bootstrap for front end
--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const { join } = require('path');
 require('dotenv').config()
 var io = require("./io")
 io(http)
+let pages = [{ url: "/about", title: "About" }]
 app.set('views', join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/dist', static("dist"))
 app.use(static("public"))
 app.get('/', (req, res) => {
-    res.render('index', { message: "from express js" })
+    res.render('index', { message: "from express js", title: "Socket/Riot Example", pages })
+});
+app.get('/about', (req, res) => {
+    res.render('about', { message: "from express js", title: "Socket/Riot Example - About", pages })
 });
 http.listen(process.env.port, () => {
     console.log(`listening on *:${process.env.port}`);
